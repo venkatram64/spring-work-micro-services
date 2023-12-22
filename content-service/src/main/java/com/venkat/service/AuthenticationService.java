a47e124d0e4d3e4ea31c57ccef6100bf47a4c319
venkatram64 <venkat.veerareddy@hotmail.com>
2023-12-22 16:06:21 +0530

    init commit

--- content-service/src/main/java/com/venkat/service/AuthenticationService.java
+++ content-service/src/main/java/com/venkat/service/AuthenticationService.java
@@ -8,14 +8,19 @@ import com.venkat.repository.UserRepository;
 import com.venkat.vo.AuthRequest;
 import com.venkat.vo.AuthResponse;
 import com.venkat.vo.UserRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationService.class);
 
     @Autowired
     private UserRepository userRepository;
@@ -40,8 +45,14 @@ public class AuthenticationService {
     }
 
     public AuthResponse authenticate(AuthRequest request){//login user
+        try {
+            //this will authenticate, internally it calls the authentication provider
+            manager.authenticate(new UsernamePasswordAuthenticationToken(request.email(), request.password()));
+        }catch(AuthenticationException exception){
+            logger.info("Authentication failed {}", exception);
+            return null;
+        }
 
-        manager.authenticate(new UsernamePasswordAuthenticationToken(request.email(), request.password()));
         var user = userRepository.findByEmail(request.email())
                 .orElseThrow();
         CustomUserDetail customUserDetail = new CustomUserDetail(user);
--- content-service/src/main/java/com/venkat/service/CustomUserDetailService.java
+++ content-service/src/main/java/com/venkat/service/CustomUserDetailService.java
@@ -3,6 +3,8 @@ package com.venkat.service;
 import com.venkat.model.CustomUserDetail;
 import com.venkat.model.User;
 import com.venkat.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,12 +15,13 @@ import java.util.Optional;
 
 @Component
 public class CustomUserDetailService implements UserDetailsService {
-
+    private static final Logger logger = LoggerFactory.getLogger(CustomUserDetailService.class);
     @Autowired
     private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.info("fetching the user {}", username);
         Optional<User> user = userRepository.findByEmail(username);
         return user.map(CustomUserDetail::new)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found in database for given email " + username));
